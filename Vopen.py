c31266aa6f070646d2d287a61a6bb0008577c0d1
edkea001 <edkea001@osloskolen.no>
2022-09-28 12:24:17 +0200

    Merge branch 'main' of https://github.com/Lovenchristian/Monsterspill

--- Vopen.py
+++ Vopen.py
@@ -1,9 +1,10 @@
 class Vopen:
 
-    def __init__(self, skade, hit_chance):
+    def __init__(self, navn, skade, hit_chance):
+        self.navn=navn
         self.skade=skade
         self.hit_chance=hit_chance
 
 alle_vopen = [Vopen(10,70),Vopen(20,40),Vopen(35,30)]
-print(Sverd.skade)
+
 
