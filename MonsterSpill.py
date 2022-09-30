ccae66c864abf1e3c079d1012bffee6d28160b1c
Lovenchristian <113507529+Lovenchristian@users.noreply.github.com>
2022-09-30 14:22:44 +0200

    commit

--- MonsterSpill.py
+++ MonsterSpill.py
@@ -17,3 +17,8 @@ while (h._spiller.hp > 0 and m._monster.hp > 0):
     h._spiller.angrip(m._monster)
     m._monster.angrip(h._spiller)
     print_hp()
+
+if h._spiller.hp <=0:
+    print("Du tapte kampen. Monsteret vant")
+elif m._monster <=0:
+    print("Gratuler du vant!")
